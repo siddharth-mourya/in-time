539eebf02b3cda395f0abf9bf100d1913550f5f0
Siddharth Mourya <mouryasiddharth98@gmail.com>
2025-02-08 08:39:37 +0000

    ee

--- src/app/component/TimerWrapper/TimerWrapper.tsx
+++ src/app/component/TimerWrapper/TimerWrapper.tsx
@@ -1,9 +1,7 @@
-import moment from "moment";
 import Timer from "../Timer/Timer";
 import styles from "./TimerWrapper.module.scss";
 
 const TimerWrapper = () => {
-  const time = moment().format("HH:mm:ss");
   return (
     <div className={styles.timerWrapper}>
       {/* Let side blur vertical column */}
